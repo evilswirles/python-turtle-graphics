01259c683e50de8e46ee91b1629d64df20e70982
evilswirles <185973850+evilswirles@users.noreply.github.com>
2025-02-10 22:24:09 -0500

    February 10

--- February 10 _ Winter Wonderland.py
+++ February 10 _ Winter Wonderland.py
@@ -4,6 +4,37 @@ wn = turtle.Screen()
 t = turtle.Turtle()
 
 #ADD CODE HERE
+wn.bgcolor("#191970")  #changes the background colour
+t.pencolor("white")
+
+def snowflake_1():
+    t.right(60)
+    t.forward(100)
+    t.penup()
+    t.back(100)
+    t.pendown()
+
+def snowflake_2():
+    t.right(60)
+    t.forward(100)
+    t.penup()
+    t.back(100)
+    t.pendown()
+
+
+i = 0
+while i < 6:
+    snowflake_1()
+    i += 1
+
+t.penup()
+t.forward(150)
+t.pendown()
+
+i = 0
+while i < 6:
+    snowflake_2()
+    i += 1
 
 #last line of code
 wn.exitonclick()
