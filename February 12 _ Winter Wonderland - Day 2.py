9e85a5c48127279706f8c74821c11f1571b95a30
evilswirles <185973850+evilswirles@users.noreply.github.com>
2025-02-12 14:12:50 -0500

    February 12

--- February 12 _ Winter Wonderland - Day 2.py
+++ February 12 _ Winter Wonderland - Day 2.py
@@ -9,11 +9,34 @@ t.pencolor("white") #colour of the pen
 t.speed(5) #speed of the turtle
 
 
-t.forward(100)
-t.back(50)
-t.right(90)
-t.forward(100)
+def snowflake_1(): #snowflake 1
+    t.right(60)
+    t.forward(100)
+    t.penup()
+    t.back(100)
+    t.pendown()
 
+
+i = 0
+while i < 6:
+    snowflake_1()
+    i += 1
+
+t.penup()
+t.forward(150)
+t.pendown()
+
+
+t.forward(60)
+t.back(30)
+t.right(90)
+t.forward(30)
+t.back(60)
+t.forward(30)
+t.right(30)
+t.forward(30)
+t.back(60)
+t.back(30)
 
 #last line of code
 wn.exitonclick()
