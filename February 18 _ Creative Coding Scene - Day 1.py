faa5f7945a3db5687655ddde51dd0157229359d6
evilswirles <185973850+evilswirles@users.noreply.github.com>
2025-02-18 14:56:00 -0500

    February 18

--- February 18 _ Creative Coding Scene - Day 1.py
+++ February 18 _ Creative Coding Scene - Day 1.py
@@ -5,10 +5,14 @@ t = turtle.Turtle()
 
 #ADD CODE HERE
 wn.bgcolor("#ADD8E6")  #changes the background colour
+t.speed(10) #speed of the turtle
+
 t.penup()
 t.right(90) #start of green grass
 t.forward(200)
 t.right(90)
+
+t.pencolor("green") #colour of the pen
 t.pendown() #pendown for drawing of grass
 t.forward(370)
 t.back(715)
@@ -17,9 +21,20 @@ t.right(90)
 t.right(90)
 t.forward(125)
 t.right(90)
-t.forward(700) # long
+t.forward(700) # longs
 t.right(90)
 t.forward(125)
+
+t.pencolor("black") #colour of the pen
+t.penup()
+t.right(90)
+t.forward(130)
+t.left(90)
+
+t.pendown() #start of cottage
+t.forward(200)
+t.right(90)
+
 
 #last line of code
 wn.exitonclick()
