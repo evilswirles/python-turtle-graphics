418f37500955f30621c513d26a6f090eb2c28f86
evilswirles <185973850+evilswirles@users.noreply.github.com>
2025-02-19 13:22:32 -0500

    February 19

---
+++ February 19 _ Creative Coding Scene - Day 2.py
@@ -0,0 +1,106 @@
+import turtle
+wn = turtle.Screen()
+#create as many turtles as you want
+t = turtle.Turtle()
+sun = turtle.Turtle()
+
+#ADD CODE HERE
+wn.bgcolor("#ADD8E6")  #changes the background colour
+t.speed(10) #speed of the turtle
+sun.speed(10) #speed of the turtle
+
+t.penup()
+t.right(90) #start of green grass
+t.forward(200)
+t.right(90)
+
+t.fillcolor("#5bb450") #tree
+t.begin_fill()
+t.pencolor("green")
+t.pendown() #pendown for drawing of grass
+t.forward(370)
+t.back(715)
+t.right(90) #rotates 3 times ("t.right(90)" x3)
+t.right(90)
+t.right(90)
+t.forward(125)
+t.right(90)
+t.forward(700) # longs
+t.right(90)
+t.forward(125)
+t.end_fill() #end fill for grass
+
+t.pencolor("black")
+t.penup()
+t.right(90)
+t.forward(120)
+t.left(90)
+
+t.pendown() #start of cottage house
+t.pencolor("#C38560")
+t.forward(200)
+t.right(90)
+t.forward(260) #
+t.right(90)
+t.forward(200)
+
+t.pencolor("green")
+t.right(90)
+t.forward(260)
+t.right(90)
+t.right(90)
+t.forward(110)
+t.left(90)
+
+t.fillcolor("#B39B84") #door frame fill
+t.begin_fill()
+t.pencolor("black") #door frame
+t.forward(55)
+t.right(90)
+t.forward(40)
+t.right(90)
+t.forward(55)
+t.back(27.5)
+t.right(90)
+t.end_fill() #end of door frame fill
+
+t.penup() #doorknob
+t.forward(12)
+t.pendown() #start of circle of doorknob
+t.circle(5)
+
+t.penup()
+t.forward(137)
+t.left(90)
+t.forward(10)
+t.left(90)
+
+t.pencolor("pink") #tiling pen colour
+t.pendown() #start of tiling
+t.forward(109)
+
+t.penup()
+t.forward(40)
+
+t.pendown()
+t.forward(110)
+t.left(90)
+
+t.penup()
+t.forward
+
+
+
+sun.penup() #yellow sun
+sun.forward(365)
+sun.left(90)
+sun.forward(300)
+
+t.fillcolor("#FFFF00") #yellow sun fill
+t.begin_fill()
+sun.pendown() #start of yellow sun
+sun.circle(50)
+t.end_fill() #end of yellow sun fill
+
+#last line of code
+wn.exitonclick()
