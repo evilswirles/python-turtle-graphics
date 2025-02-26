8a9cb16bbb3f5dc8d471a8248921fdd3f9387a06
evilswirles <185973850+evilswirles@users.noreply.github.com>
2025-02-26 17:32:00 -0500

    February 26

---
+++ Unit 1 - Data Types & Variables/February 26 _ Investigate Strings.py
@@ -0,0 +1,41 @@
+#print ("\tDoh\nDohDoh:" )
+
+#print ("Hello")
+#print ("My name\nis")
+#print ("name")
+
+#print ("\\t\tx\\n\nx")
+
+#print ("abc"*5 )
+
+#print ("cup" + "cake")
+
+#print ("cup " + "cake")
+
+#print ("hello")
+#print ("there")
+
+#print ("I said \'get to work!!!!\' ")
+
+#print "hello"
+#print "there"
+
+#print("I am so smart")
+
+#print ("  'help' ")
+
+#print ("hithere)
+
+#print ("HI")
+
+#print("hi"*7)
+
+#print("Hihihihihihihihihi")
+
+#print("Sorry I’m late\n"*3)
+
+#print("'Hi there'")
+
+#print("//\\")
+
+
