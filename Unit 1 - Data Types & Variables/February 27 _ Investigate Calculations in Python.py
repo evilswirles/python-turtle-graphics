7ca8f075d3dbb2139c9c2e1afd347d35d08fb02b
evilswirles <185973850+evilswirles@users.noreply.github.com>
2025-02-28 17:43:22 -0500

    Upload .py files from past two classes (27 and 28)

---
+++ Unit 1 - Data Types & Variables/February 27 _ Investigate Calculations in Python.py
@@ -0,0 +1,46 @@
+#print(80/2)
+
+#print(40**5)
+
+#print(40*4)
+
+#print(4==8)
+
+#print(3!=10)
+
+#print(30==30)
+
+#print(40%8)
+
+
+# Part B- Using Calculations in Python programs
+
+#num1=input("Enter a number between 1 and 100")
+#num2=input("Enter another number between 1 and 100")
+
+#print("Number 1 + Number 2=",num1+num2)
+
+#-----------
+
+#num1=int(input("Enter a number between 1 and 100:"))
+#num2=int(input("Enter another number between 1 and 100:"))
+
+#print("Number 1 + Number 2=",num1+num2)
+
+
+#Part C- Try It Out - Input Calculations & Variables
+
+#a1=int(input("Enter 5"))
+#a2=int(input("Enter 12"))
+#print("a1 + a2=",a1+a2) #c
+#print(17 - 5) #d : c-a
+#print(5 + 12 + 17 + 12) #e : a+b+c+d
+
+
+#---Write a program that asks a user to enter their age---
+#---Their age should get multiplied by 12 and then output to the screen---
+
+age1=int(input("What is your age? Please enter below:\n>>> "))
+age1 = age1 * 12
+
+print("Your age is ",age1)
