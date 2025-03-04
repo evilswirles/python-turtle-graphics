6ecdcdd2659a4eb66c66b7a65aa04c7897a7b753
evilswirles <185973850+evilswirles@users.noreply.github.com>
2025-03-03 22:35:14 -0500

    Updated

---
+++ Unit 1 - Data Types & Variables/ICD2O1_b-ryan_march-3_practice-arithmetic-expressions.py
@@ -0,0 +1,29 @@
+import math
+
+print("2 + 3 = " , 2+3)
+
+print("3 * 4 + 2 = " , 3*4+2)
+
+print("3 * 4 + 2.0 = " , 3*4+2.0)
+
+print("5 / 10 = " , 5/10)
+
+print("5 / 10.0 = " , 5/10.0)
+
+print("5 / 9 = " , 5/9)
+
+print("2 ** 4 = " , 2**4)
+
+print(abs((-2) ** 4))
+
+print(math.pow(2, 4))
+
+#print(sqrt(4))
+
+print(math.sqrt(4))
+
+print(math.cos(0))
+
+print(math.pi)
+
+print(math.sin(math.pi / 2))
---
+++ Unit 1 - Data Types & Variables/March 3 _ Dividing Numbers.py
@@ -0,0 +1,6 @@
+numStudents = 4
+
+numMints = 11
+
+print = int(input("Number of students in your team:\n>>>"))
+
